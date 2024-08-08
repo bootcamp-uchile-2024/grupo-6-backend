9f9b0e99830d4fe54a8a0069351a562acf8e10da
seba <seba.flores.ram@gmail.com>
2024-08-07 20:26:07 -0400

    Creacion modulo products

---
+++ src/products/products.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Response } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  // @Post()
+  // create(@Body() createProductDto: CreateProductDto) {
+  //   return this.productsService.create(createProductDto);
+  // }
+
+  @Get()
+  nombreEpica() {
+    return this.productsService.nombreEpica();
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.productsService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  //   return this.productsService.update(+id, updateProductDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.productsService.remove(+id);
+  // }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+  // create(createProductDto: CreateProductDto) {
+  //   return 'This action adds a new product';
+  // }
+
+  // findAll() {
+  //   return `This action returns all products`;
+  // }
+
+  nombreEpica() {
+    return `Este modulo corresponde a la epica "Obtencion de Producto".`;
+  }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} product`;
+  // }
+
+  // update(id: number, updateProductDto: UpdateProductDto) {
+  //   return `This action updates a #${id} product`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} product`;
+  // }
+}

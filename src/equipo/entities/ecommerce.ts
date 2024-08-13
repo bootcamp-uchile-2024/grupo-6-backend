722cf9451bb30d9b0eb47b2c09f6ee961ba3cb97
seba <seba.flores.ram@gmail.com>
2024-08-12 20:32:14 -0400

    servicio y controlador  equipo - parte iii y iv

---
+++ src/equipo/entities/ecommerce.ts
@@ -0,0 +1,9 @@
+export class Ecommerce {
+    constructor(
+    public nombre: string,
+    public descripcion: string,
+    public tipoEcommerce: string,
+    public objetivoGeneral: string,
+    public objetivosEspecificos: string[],
+    ){}
+}

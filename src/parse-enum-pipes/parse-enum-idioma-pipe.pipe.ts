4734498c79d862e8aa1403b166b474245089be9b
Nicole Carvajal <nicole.carvajalg@gmail.com>
2024-09-08 16:18:18 -0300

    Corrección Módulo Products

--- src/parse-enum-pipes/parse-enum-idioma-pipe.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-@Injectable()
-export class ParseEnumEditorialPipePipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return value;
-  }
-}

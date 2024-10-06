4f889e57f56170a37b44fa6301f9bacf26c2d28d
silvano <snoxdev@github.com>
2024-10-06 16:18:20 -0300

    Arreglos_Shoppingcart

--- src/shoppingcart/dto/update-shoppingcart.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateShoppingcartDto } from './create-shoppingcart.dto';
-
-export class UpdateShoppingcartDto extends PartialType(CreateShoppingcartDto) {}

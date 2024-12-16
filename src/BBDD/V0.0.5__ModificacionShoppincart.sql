6a48b83033e94803cd7aeedef24c73db0607de19
snoxdev <jsmoing@gmail.com>
2024-12-16 17:07:52 -0300

    part 3

--- src/BBDD/V0.0.5__ModificacionShoppincart.sql
+++ src/BBDD/V0.0.5__ModificacionShoppincart.sql
@@ -32,21 +32,26 @@ UPDATE carrito SET descuento = 2 WHERE isbn_libro = '9783836526470';
 UPDATE carrito SET descuento = 1 WHERE isbn_libro = '9783836597036';
 UPDATE carrito SET descuento = 1 WHERE isbn_libro = '9788419785855';
 
+ALTER TABLE carrito RENAME COLUMN usuario_id TO carrito_id;
+
+ALTER TABLE carrito DROP FOREIGN KEY fk_usuario_id;
+ALTER TABLE carrito ADD CONSTRAINT fk_carrito_id FOREIGN KEY (carrito_id) REFERENCES otra_tabla(carrito_id);
+
 CREATE TABLE carrito_informacion (
-    id_carrito INT AUTO_INCREMENT,
+    id_carrito INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT,
     fecha_actualizacion DATE,
     precio_total INT,
-    PRIMARY KEY (id_carrito, usuario_id),
+    estado ENUM ('activo', 'completado', 'cancelado') DEFAULT 'activo',
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
-INSERT INTO carrito_informacion (id_carrito, usuario_id, fecha_actualizacion, precio_total) VALUES 
-(1, 2, '2024-12-15', 112500),
-(2, 5, '2024-12-15', 18000),
-(3, 8, '2024-12-15', 101890),
-(4, 12, '2024-12-15', 118890),
-(5, 19, '2024-12-15', 52800);
+INSERT INTO carrito_informacion (id_carrito, usuario_id, fecha_actualizacion, precio_total, estado) VALUES 
+(1, 2, '2024-12-15', 112500, 'activo'),
+(2, 5, '2024-12-15', 18000, 'activo'),
+(3, 8, '2024-12-15', 101890, 'activo'),
+(4, 12, '2024-12-15', 118890, 'activo'),
+(5, 19, '2024-12-15', 52800, 'activo');
 
 CREATE TABLE purchase (
     id_compra INT,
